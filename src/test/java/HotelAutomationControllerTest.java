263d697a1b46f92e3d26c7a2567dddd55d6d2ac5
milind.takawale <milind.takawale@arista.com>
2019-03-06 20:58:46 +0530

    More tests, unit assetion demanded Light class, corridor light assertion demanded Corridor class and improved assertion in existing test

--- src/test/java/HotelAutomationControllerTest.java
+++ src/test/java/HotelAutomationControllerTest.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class HotelAutomationControllerTest {
+class HotelAutomationControllerTestTest {
+
+    @BeforeEach
+    void setUp() {
+       Hotel hotel = new Hotel(2, 2, 2);
+    }
+
+    @Test
+    void hotelCanHaveMultipleFloors() {
+        Hotel hotel = new Hotel(2, 0, 0);
+        Assertions.assertThat(hotel.getFloors().size()).isEqualTo(2);
+    }
+
+    @Test
+    void eachFloorCanHaveMultipleMainCorridors() {
+        Hotel hotel = new Hotel(2, 3, 0);
+        Assertions.assertThat(hotel.getFloors().get(0).getCorridors().size()).isEqualTo(3);
+    }
+
 }
